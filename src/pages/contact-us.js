2f12a5ae25e2a67e1fe0c15565e2a1c766c6fb4a
aguriken <ohagurokenichi@yahoo.co.jp>
2020-04-12 17:06:35 +0900

    fix string hidden

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -77,7 +77,7 @@ const ContactUs = (props) => {
           method="POST"
           data-netlify="true"
           action="/contact-success">
-            <input type="hedden" name="form-name" value="contact-us" />
+            <input type="hidden" name="form-name" value="contact-us" />
           {props.data.prismic.allContact_pages.edges[0].node.form_fields.map((field, i) => {
             if (field.field_type === 'textarea') {
               return (
